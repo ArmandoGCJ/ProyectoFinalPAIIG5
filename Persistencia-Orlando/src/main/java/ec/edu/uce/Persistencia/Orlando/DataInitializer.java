07eeb092a30c556333eee268fd5c6cce1dc4756f
EdgarTipan <evtipanl1@uce.edu.ec>
2024-07-27 16:12:33 -0500

    Corregido

--- Persistencia-Orlando/src/main/java/ec/edu/uce/Persistencia/Orlando/DataInitializer.java
+++ Persistencia-Orlando/src/main/java/ec/edu/uce/Persistencia/Orlando/DataInitializer.java
@@ -1,40 +1,24 @@
 package ec.edu.uce.Persistencia.Orlando;
 
-import ec.edu.uce.Persistencia.Orlando.model.Cliente;
 import ec.edu.uce.Persistencia.Orlando.model.Etapa;
 import ec.edu.uce.Persistencia.Orlando.model.Producto;
-import ec.edu.uce.Persistencia.Orlando.service.ClienteService;
-import ec.edu.uce.Persistencia.Orlando.service.PedidoService;
 import ec.edu.uce.Persistencia.Orlando.service.ProductoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Component
 public class DataInitializer implements CommandLineRunner {
 
-    private final ClienteService clienteService;
     private final ProductoService productoService;
-    private final PedidoService pedidoService;
 
     @Autowired
-    public DataInitializer(ClienteService clienteService, ProductoService productoService, PedidoService pedidoService) {
-        this.clienteService = clienteService;
+    public DataInitializer(ProductoService productoService) {
         this.productoService = productoService;
-        this.pedidoService = pedidoService;
     }
 
     @Override
     public void run(String... args) throws Exception {
-        // Agregar un cliente
-        Cliente cliente = new Cliente();
-        cliente.setNombre("Juan Perez");
-        cliente.setContrasena("password123");
-        cliente.setRol("ADMIN");
-        clienteService.guardarCliente(cliente);
 
         // Agregar un producto con etapas
         Producto producto = new Producto();
@@ -77,9 +61,5 @@ public class DataInitializer implements CommandLineRunner {
         producto2.getEtapas().add(etapa4);
 
         productoService.guardarProducto(producto2);
-
-        // Agregar un pedido
-        List<Long> productosIds = Arrays.asList(producto.getId(), producto2.getId());
-        pedidoService.guardarPedido(cliente.getId(), productosIds);
     }
 }
